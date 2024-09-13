906650e98b25a7d9194dc4c7d5d7d9ab5919072a
David Buckley <david.buckley@shopify.com>
2024-09-13 16:47:39 -0400

    decode files

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,1 +1,32 @@
-IyMgZmlyc3RfdGVuX2ZpYm9uYWNjaSgpCiMjIEZpbmRzIHRoZSAxc3QgMTAgbnVtYmVycyBpbiB0aGUgRmlib25hY2NpIFNlcXVlbmNlCiMjIFNhdmVzIGVhY2ggbnVtYmVyIGFzIGVsZW1lbnRzIGluIGFuIGFycmF5CiMjIFJldHVybnMgdGhhdCBhcnJheQpkZWYgZmlyc3RfdGVuX2ZpYm9uYWNjaQogIFtdCmVuZAoKIyMgcHJvY2Vzc19hcnJheShudW1iZXJzKQojIyBBcmdzOiBBbiBhcnJheSBvZiBudW1iZXJzCiMjIFJldmVyc2VzIHRoZSBhcnJheQojIyBNdWx0aXBsaWVzIGVhY2ggbnVtYmVyIGJ5IGl0J3MgaW5kZXggKiAyCiMjIFN1bXMgdGhlIG51bWJlcnMgaW4gdGhlIGFycmF5IHRvZ2V0aGVyIGFuZCByZXR1cm5zIGl0CmRlZiBwcm9jZXNzX2FycmF5KG51bWJlcnMpCiAgMAplbmQKCiMjIHByaW50X2FzY2lpX2NoYXJhY3RlcigpCiMjIEFyZ3M6IEEgbnVtYmVyCiMjIFJlbW92ZXMgYW55IGR1cGxpY2F0ZSBkaWdpdHMgaW4gdGhlIG51bWJlcgojIyBSZXBsYWNlcyB0aGVtIHdpdGggYSBzaW5ndWxhciBpbnN0YW5jZSBpZTogMTEwID0gMTAKIyMgTXVsdGlwbGllcyB0aGF0IG51bWJlciBieSA0CiMjIFJldHVybnMgdGhlIEFTQ0lJIGNoYXJhY3RlciB0aGF0IGhhcyB0aGUgc2FtZSB2YWx1ZSBvZiB0aGUgbnVtYmVyCmRlZiBwcmludF9hc2NpaV9jaGFyYWN0ZXIobnVtYmVyKQogICAgJycKZW5kCgpmaWJvbmFjY2kgPSBmaXJzdF90ZW5fZmlib25hY2NpKCkKc3VtbWVkID0gcHJvY2Vzc19hcnJheShmaWJvbmFjY2kpCmFzY2lpID0gcHJpbnRfYXNjaWlfY2hhcmFjdGVyKHN1bW1lZCkKCnB1dHMgYXNjaWkK
+## first_ten_fibonacci()
+## Finds the 1st 10 numbers in the Fibonacci Sequence
+## Saves each number as elements in an array
+## Returns that array
+def first_ten_fibonacci
+  []
+end
+
+## process_array(numbers)
+## Args: An array of numbers
+## Reverses the array
+## Multiplies each number by it's index * 2
+## Sums the numbers in the array together and returns it
+def process_array(numbers)
+  0
+end
+
+## print_ascii_character()
+## Args: A number
+## Removes any duplicate digits in the number
+## Replaces them with a singular instance ie: 110 = 10
+## Multiplies that number by 4
+## Returns the ASCII character that has the same value of the number
+def print_ascii_character(number)
+    ''
+end
+
+fibonacci = first_ten_fibonacci()
+summed = process_array(fibonacci)
+ascii = print_ascii_character(summed)
+
+puts ascii
